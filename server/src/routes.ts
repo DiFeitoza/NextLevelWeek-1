9c5b9da2adaa985834b8a49c195ca8eaac3e393e
difeitoza <diegofeitozabr@gmail.com>
2020-11-02 22:09:25 -0300

    routes for get items and post points

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,80 +1,70 @@
 import express, { Router } from 'express';
+// importa connection como knex
+import knex from './database/connection';
 
 const routes = express.Router();
 
-const users = [
-  {
-    id: '0',
-    name: 'diego'
-  },
-  {
-    id: '1',
-    name: 'tiago'
-  },
-  {
-    id: '2',
-    name : 'junior'
-  }
-];
+routes.get('/items', async (request, response) => {
+  const items = await knex('items').select('*');
 
-routes.get('/html', (request, response) => {
-  console.log("enviando string como html");
-  return response.send("<!DOCTYPE HTML><head></head><body><h1><div style = 'color:#00f; text-align: center'>Olá Rianny</div></h1><div><img style = 'width: 1000px; display: block; margin-left: auto; margin-right: auto;' src='https://i.pinimg.com/originals/e5/7e/87/e57e871c830c9ec1e1ae51c07a9f7c56.jpg'></div></body>");
+  const serializedItems = items.map(item => {
+    return {
+      id: item.id,
+      title: item.title,
+      //EC2015 incluiu o acento grave como forma de manupular strings. ex. ${}
+      image_url: `http://localhost:3131/uploads/${item.image}`
+    }
+  });
+
+  return response.json(serializedItems);
 });
 
-/* routes.get('/users', (request, response) => {
-  console.log('List users');
-  // query permite que eu receba um par 'chave':'valor' na mesma rota, sem ser por parâmetro de recurso
-  const valueSearch = request.query.search;
+routes.post('/points', async (request, response) => {
+  // const data = request.body;
+  
+  /* recurso de desestruturação do javascript, mesmo que escrever:
+  const name = request.body.name;
+  const email = request.body.name;
+  ... */
 
-  const filteredUsers = valueSearch ?
-    users.filter(
-      user => user.name.includes(String(valueSearch))
-    ) : users;
+  /* short sintaxe, p.ex. "name: 'name'"; quando o nome da propriedade é igual ao nome da variável do objeto, podemos omitir a propriedade. */
 
-  return response.json({
-    filteredUsers
-  })
-}); */
+  const {
+    // image,
+    name,
+    email,
+    whatsapp,
+    longitude,
+    latitude,
+    city,
+    uf,
+    items
+  } = request.body;
 
-routes.get('/users', (request, response) => {
-  console.log('GET /users');
-  return response.json(users);
-});
+  // trx (transaction), é uma forma de garantia de que as duas transações no banco ocorram de forma atômica. Se uma falhar a outra é cancelada (roolback).
+  const trx = await knex.transaction();
 
-routes.get('/users/:id', (request, response) => {
-  // preciso colocar ':' antes do recurso para poder acessá-lo com o método request.params
-  console.log('GET /users/' + request.params.id);
-  return response.json(users[Number(request.params.id)])
-});
+  const insertedIds = await trx('points').insert({
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    longitude,
+    latitude,
+    city,
+    uf,
+  });
 
-routes.post('/users', (request, response) => {
-  console.log('POST /users' + request.body);
-  return response.json(users.push(request.body) ? users : "fail");
-});
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id: insertedIds[0],
+    };
+  });
 
-routes.put('/users/:id',  (request, response) => {
-  let id = request.params.id;
-  let data = request.body;
-  console.log('PUT /user/' + id + ' dados: ' + data);
+  await trx('points_items').insert(pointItems);
 
-  let idxUser = users.findIndex(
-      (user) => { return user.id === id }
-  );
- 
-  return response.json(
-    idxUser != -1 ? users[idxUser].name = data.name : 'not find'
-  );
-});
-
-routes.delete('/users/:id', (request, response) => {
-  console.log('DELETE /users/' + request.params.id);
-
-  let indDel = users.findIndex(
-    (user) => { return user.id === request.params.id }
-  );
-
-  return response.json(users.splice(indDel, 1));
+  return response.json({ succes: true });
 });
 
 export default routes;
